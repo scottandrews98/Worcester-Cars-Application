5fbaeab2e289dbaaf3f58a7965e0484a9371fcc9
Scott Andrews <scottandrews490@gmail.com>
2020-02-06 17:34:55 +0000

    fixed lightbox implementation

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js([
+   'resources/js/app.js',
+   'node_modules/lightbox2/dist/js/lightbox.min.js'], 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
